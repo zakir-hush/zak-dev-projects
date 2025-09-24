868997ec9f8c763f19739546a8a59c6795c5a8fc
zakir-hush <zakir.hush@gmail.com>
2025-09-24 15:27:27 +0800

    updated create-aws-sg.tf code with additional inbound rule

--- terraform/aws-create-sg/create-aws-sg.tf
+++ terraform/aws-create-sg/create-aws-sg.tf
@@ -23,6 +23,14 @@ resource "aws_security_group" "new_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+    ingress {
+    description      = "Allow RDP"
+    from_port        = 3389
+    to_port          = 3389
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     description      = "Allow all outbound traffic"
     from_port        = 0
